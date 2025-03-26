1d8c0fb4580df1ffa2f86854f501f9ca184e73fa
kyonav <tokyonav@proton.me>
2025-03-26 07:15:31 -0300

    Upd: created global var userInputInt

--- ConvertInput.cs
+++ ConvertInput.cs
@@ -2,8 +2,11 @@ namespace XDiAMOND;
 
 class ConvertInput
 {
-    public static int ConvertToInt32()
+    public static int userInputInt;
+    public static int ConvertToInt32(string userInputString)
     {
+        userInputInt = Convert.ToInt32(userInputString);
 
+        return userInputInt;
     }
 }
