f004c62bdf434e9891a18574e2cfdd27be9bd81c
kyonav <tokyonav@proton.me>
2025-03-26 07:37:58 -0300

    Upd: added invalid input log

--- LogToUser.cs
+++ LogToUser.cs
@@ -2,5 +2,10 @@ namespace XDiAMOND;
 
 class LogToUser
 {
-    
+    public static void OnlyNumbersAllowed()
+    {
+        Console.Clear();
+        
+        Console.WriteLine("Invalid input. Only numbers are allowed.");
+    }
 }
