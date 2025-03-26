34cd408fea6c7d6c0cbd01586fa2b86cdcabe6f1
kyonav <tokyonav@proton.me>
2025-03-26 07:45:35 -0300

    Upd: created NullInputDetected func

--- LogToUser.cs
+++ LogToUser.cs
@@ -2,10 +2,17 @@ namespace XDiAMOND;
 
 class LogToUser
 {
-    public static void OnlyNumbersAllowed()
+    public static void StringInputDetected()
     {
         Console.Clear();
-        
+
         Console.WriteLine("Invalid input. Only numbers are allowed.");
+    }
+
+    public static void NullInputDetected()
+    {
+        Console.Clear();
+
+        Console.WriteLine("Null input detected. Only numbers allowed.");
     }
 }
