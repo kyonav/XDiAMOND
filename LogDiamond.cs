b2907dde898612a28dda7992496f8794533a8869
kyonav <tokyonav@proton.me>
2025-03-26 09:31:56 -0300

    Del: PrintX* func parameters

--- LogDiamond.cs
+++ LogDiamond.cs
@@ -4,11 +4,11 @@ namespace XDiAMOND;
 
 class LogDiamond
 {
-    public static void PrintXDiamondTop(int userInputIntOdd)
+    public static void PrintXDiamondTop()
     {
-        int spaces = (userInputIntOdd / 2);
+        int spaces = (ConvertInput.userInputInt / 2);
 
-        for (int lineOfDiamond = 1; lineOfDiamond <= userInputIntOdd; lineOfDiamond += 2)
+        for (int lineOfDiamond = 1; lineOfDiamond <= ConvertInput.userInputInt; lineOfDiamond += 2)
         {
             for (int spacesBeforeX = 0; spacesBeforeX < spaces ; spacesBeforeX++)
             {
@@ -26,12 +26,12 @@ class LogDiamond
         } 
     }
 
-    public static void PrintXDiamondBottom (int userInputIntOdd){
+    public static void PrintXDiamondBottom (){
         
-        int spacesQtt = (userInputIntOdd / 2);
+        int spacesQtt = (ConvertInput.userInputInt / 2);
         int oldSpacesQtt = spacesQtt;
 
-        for (int lineOfDiamond = (userInputIntOdd - 2); lineOfDiamond >= 1; lineOfDiamond -= 2)
+        for (int lineOfDiamond = (ConvertInput.userInputInt - 2); lineOfDiamond >= 1; lineOfDiamond -= 2)
         {
             for (int spacesBeforeX = spacesQtt; spacesBeforeX >= oldSpacesQtt; spacesBeforeX--)
             {
