58afda12f1ee41331d0dbc23313e4337b1173e36
kyonav <tokyonav@proton.me>
2025-03-26 03:40:59 -0300

    calling other files classes

--- Program.cs
+++ Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        ApplicationStart();
+    }
+
+    public static void ApplicationStart()
+    {
+        string userInput = AskForUserInput.UserInput();
+
+        int userInputInt = ConvertNumber.ConvertToInt(userInput);
+
+        bool IsUserInputIntOdd = IsOddOrEven.CheckIfOdd(userInputInt);
+
+        if (IsUserInputIntOdd){
+            DiamondLog.PrintXDiamond(userInputInt);
+        }
     }
 }
