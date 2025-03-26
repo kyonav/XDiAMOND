330322648cc597c816cbd0ab2c760103211e7a35
kyonav <tokyonav@proton.me>
2025-03-26 09:30:16 -0300

    Del: ConvertToInt32 func parameter

--- ConvertInput.cs
+++ ConvertInput.cs
@@ -3,9 +3,9 @@ namespace XDiAMOND;
 class ConvertInput
 {
     public static int userInputInt;
-    public static int ConvertToInt32(string userInputString)
+    public static int ConvertToInt32()
     {
-        userInputInt = Convert.ToInt32(userInputString);
+        userInputInt = Convert.ToInt32(RequestUserInput.userInputString);
 
         return userInputInt;
     }
