ce00914b679a4c22b51d93f1c96ed4107dad363c
kyonav <tokyonav@proton.me>
2025-03-26 11:08:52 -0300

    Upd: juicy formatting for prettier output

--- LogToUser.cs
+++ LogToUser.cs
@@ -5,6 +5,8 @@ class LogToUser
 
     public static void TypeANumber()
     {
+        Console.WriteLine("________________________________");
+        Console.WriteLine();
         Console.WriteLine("Type an odd integer number!");
         Console.Write("> ");
     }
@@ -13,25 +15,35 @@ class LogToUser
     {
         Console.Clear();
 
+        Console.WriteLine("_____________________________________________");
+        Console.WriteLine();
         Console.WriteLine("Invalid input. Only numbers are allowed.");
+        Console.WriteLine("_____________________________________________");
     }
 
     public static void NullInputDetected()
     {
         Console.Clear();
 
+        Console.WriteLine("_______________________________________________");
+        Console.WriteLine();
         Console.WriteLine("Null input detected. Only numbers allowed.");
+        Console.WriteLine("_______________________________________________");
     }
     
     public static void InputIsEven()
     {
         Console.Clear();
 
+        Console.WriteLine("__________________________________________________________________________________");
+        Console.WriteLine();
         Console.WriteLine($"The number {ConvertInput.userInputInt} is even. Try again with a odd number.");
+        Console.WriteLine("__________________________________________________________________________________");
     }
 
     public static void ContinueOrNot()
     {
+        Console.WriteLine("____________________________________________");
         Console.WriteLine();
         Console.WriteLine("Do you wanna redraw the triangle? (y/*)");
         Console.Write("> ");
@@ -42,6 +54,9 @@ class LogToUser
     {
         Console.Clear();
 
+        Console.WriteLine("____________________________________________________");
+        Console.WriteLine();
         Console.WriteLine("User chose not to continue. Quitting program...");
+        Console.WriteLine("____________________________________________________");
     }
 }
